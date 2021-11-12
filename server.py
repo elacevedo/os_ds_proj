e20c69bd2261dc798c6ccae4a4d477a388e28d83
sergio <sergiozy6693@gmail.com>
2021-11-11 20:35:45 -0600

    Example of entering and removing from database.

--- server.py
+++ server.py
@@ -5,11 +5,31 @@ class hi:
 	def hi(self):
 		return 'hello'
 
-daemon = Pyro4.Daemon(host="ipaddress") #enter ipaddress of local machine. 
+# Example of entering and removing from database 
+#	def __init__ (self, name):
+#		self.name = name
+#	def visit(self, database):
+#		print (name)
+#		self.deposit(database)
+#		self.retrieve(database)
+#
+#	def deposit(self, database)
+#		print(database.listitems())
+#		item = input("Type your item".strip)
+#		if item: 
+#			database.store(self.name, item)
+#	def retrieve(self, database):
+#		print(database.listitems())
+#		item = input("Type you item".strip)
+#		if item:
+#			database.take(self.name, item)
 
-uri = daemon.register(hi(),"server")
-ns = Pyro4.locateNS()
-ns.register('obj',uri)
+
+daemon = Pyro4.Daemon()
+
+uri = daemon.register(hi)
+#ns = Pyro4.locateNS()
+#ns.register('obj',uri)
 print(uri) 
 
 daemon.requestLoop()

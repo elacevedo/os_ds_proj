cd3cbad2bdaf10a11d3fcb716fbfb68dda357b7a
xsurgefl7 <31364681+xsurgefl7@users.noreply.github.com>
2021-11-18 18:15:05 -0600

    calls with queue

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import Pyro4
 
-o = Pyro4.Proxy("PYRO:server@ipaddress:port") #enter ip address of server along with port. 
+o = Pyro4.Proxy("PYRO:server@ipaddress:55555") #enter ip address of server.
 
-print(o.hi())
+print(o.putWork("test"))
+print(o.getwork())
--- server.py
+++ server.py
@@ -1,33 +1,38 @@
 import Pyro4
 import queue
-from productDB import ProductDB
+#from productDB import ProductDB
+
+#@Pyro4.expose
+#@Pyro4.behavior(instance_mode="single") #register object to be processes concurrently. 
+#class hi:
+#	def hi(self):
+#		return 'hello'
+
 
 @Pyro4.expose
 @Pyro4.behavior(instance_mode="single") #register object to be processes concurrently. 
-class hi:
-	def hi(self):
-		return 'hello'
-
-class Dispatcher_Fetch(object):
+class Dispatcher(object):
 	def __init__(self):
-		self.fetch_all = queue.Queue()
+		self.data = queue.Queue()
 		self.resultqueue = queue.Queue()
 	def putWork(self, item):
-		self.fetch_all.put(item)
+		self.data.put(item)
+		print("item in queue")
 	def getwork(self, timeout = 5):
 		try: 
-			return self.fetch_all.get(block=True, timeout=timeout)
+			return self.data.get(block=True, timeout=timeout)
 		except queue.Empty:
 			raise ValueError("no items in queue")
 	def putResult(self, item):
 		self.resultqueue.put(item)
+		print("item in results queue")
 	def getResult(self, timeout=5):
 		try: 
 			return self.resultqueue.get(block=True, timeout=timeout)
 		except queue.Empty:
 			raise ValueError("no result available")
 	def workqueueSize(self):
-		return self.fetch_all.qsize()
+		return self.data.qsize()
 	def resultQueueSize(self):
 		return self.resultqueue.qsize()
 
@@ -51,11 +56,12 @@ class Dispatcher_Fetch(object):
 #			database.take(self.name, item)
 
 
-daemon = Pyro4.Daemon()
+daemon = Pyro4.Daemon(host="ipaddress", port=55555) #insert ip address of server, since it will be static 
+													#added static port
 
-uri = daemon.register(hi)
-#ns = Pyro4.locateNS()
-#ns.register('obj',uri)
-print(uri) 
+uri = daemon.register(Dispatcher(),"server")
+ns = Pyro4.locateNS()
+ns.register('obj',uri)
+print(uri)
 
 daemon.requestLoop()

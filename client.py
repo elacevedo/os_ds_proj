e4171c4c9b78729e7ed4cd6a9c51c2c2abb3765d
xsurgefl7 <31364681+xsurgefl7@users.noreply.github.com>
2021-11-19 22:02:55 -0600

    worker code

--- client.py
+++ client.py
@@ -3,4 +3,19 @@ import Pyro4
 o = Pyro4.Proxy("PYRO:server@ipaddress:55555") #enter ip address of server.
 
 print(o.putWork("test"))
-print(o.getwork())
+#print(o.getwork())
+
+
+item = o.getwork()
+
+#worker code
+def processdatabase():
+	o.putResult(item + "1")
+	print(o.getResult())
+
+
+def main():
+	processdatabase() 
+
+if __name__ == "__main__":
+    main()
